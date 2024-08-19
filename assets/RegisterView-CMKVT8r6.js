774559b624c80c8503620dd5ba9bea059ebe708f
liusuipin <liuprogramming@gmail.com>
2024-08-19 22:18:02 +0700

    Merge branch 'main' of https://github.com/depinliu/apin

---
+++ assets/RegisterView-CMKVT8r6.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/AuthContentComponent-QEcqs2CB.js","assets/index-Clbh7sEY.js","assets/index-Ckz6VG_K.css"])))=>i.map(i=>d[i]);
+import{d as o,u as t,o as n,e as _,_ as s}from"./index-Clbh7sEY.js";const p={__name:"RegisterView",setup(a){const e=_(()=>s(()=>import("./AuthContentComponent-QEcqs2CB.js"),__vite__mapDeps([0,1,2])));return(r,c)=>(n(),o(t(e),{isLoginPage:!1}))}};export{p as default};
