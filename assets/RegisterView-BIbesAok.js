219a2b61606e4a7b2c9d935c566d8cc0d5db70b1
liusuipin <liuprogramming@gmail.com>
2024-08-20 23:42:55 +0700

    new

---
+++ assets/RegisterView-BIbesAok.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/AuthContentComponent-A7bbewHb.js","assets/index-Boz_6IjS.js","assets/index-h9VtKMMu.css"])))=>i.map(i=>d[i]);
+import{d as o,u as t,o as n,e as _,_ as s}from"./index-Boz_6IjS.js";const p={__name:"RegisterView",setup(a){const e=_(()=>s(()=>import("./AuthContentComponent-A7bbewHb.js"),__vite__mapDeps([0,1,2])));return(r,c)=>(n(),o(t(e),{isLoginPage:!1}))}};export{p as default};
